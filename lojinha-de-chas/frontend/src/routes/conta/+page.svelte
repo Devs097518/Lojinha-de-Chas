ee497919a12142cc9a39228f50f0c9b098d8d7bd
Dayvson <dayvsonnlacerda360@gmail.com>
2025-10-02 16:00:13 -0300

    Parte CRUD aparecendo no front

--- lojinha-de-chas/frontend/src/routes/conta/+page.svelte
+++ lojinha-de-chas/frontend/src/routes/conta/+page.svelte
@@ -5,7 +5,12 @@
     let nome: string = "";
     let endereco: string = "";
     let resultado: any = {};
-    let userCode : string = '4xhzst';
+    let userCode: string = "";
+
+    let conta: string = "flex";
+    let gerar: string = "none";
+    let acessar: string = "none";
+    let dados: string = "none";
 
     let itens = [
         {
@@ -27,7 +32,10 @@
         try {
             const idCriado = Math.random().toString(36).substring(2, 8);
             const sacola: string[] = [];
+            userCode = idCriado;
+            alert(userCode);
 
+            console.log("postado com sucesso!", idCriado);
             let dadosSoltos = await fetch("http://localhost:3000/post", {
                 method: "POST",
                 headers: {
@@ -43,15 +51,17 @@
                     },
                 }),
             });
-            console.log("postado com sucesso!");
         } catch (error) {
             console.log("erro ao acessar os dados", error);
         }
     }
 
+
     async function read() {
         try {
-            let dadosSoltos = await fetch(`http://localhost:3000/acessar/${userCode}`);
+            let dadosSoltos = await fetch(
+                `http://localhost:3000/acessar/${userCode}`,
+            );
             let dadosAgrupados = await dadosSoltos.json();
 
             resultado = dadosAgrupados;
@@ -59,6 +69,14 @@
         } catch (error) {
             console.log("erro ao acessar os dados", error);
         }
+    }
+
+
+    let trocarTela = (info: string) => {
+        info == "acessar" ? (acessar = "flex") : (acessar = "none");
+        info == "gerar" ? (gerar = "flex") : (gerar = "none");
+        info == "dados" ? (dados = "flex") : (dados = "none");
+        info == "conta" ? (conta = "flex") : (conta = "none");
     };
 </script>
 
@@ -69,29 +87,77 @@
         </nav>
 
         <article>
-            <p>essa daqui é a minha conta</p>
+            <div id="conta" style="display:{conta};">
+                <h1>Escolha</h1>
+                <button on:click={() => trocarTela("acessar")}
+                    >Acessar conta</button
+                >
+                <button on:click={() => trocarTela("gerar")}>Criar conta</button
+                >
+            </div>
 
-            <div>
+
+
+            <div id="criar" style="display:{gerar};">
                 <h1>Criar Conta</h1>
-                <input type="text" bind:value={nome} /> <br />
-                <input type="text" bind:value={endereco} /> <br />
-                <button on:click={criar} on:click={read}>postar</button>
+
+
+                <table>
+                    <thead>
+                        <tr>
+                            <td>nome</td>
+                            <td style="padding:0"><input type="text" bind:value={nome} class="inputCriar"/> <br /></td>
+                        </tr>
+                        <tr>
+                            <td>endereço</td>
+                            <td style="padding:0"><input type="text" bind:value={endereco} class="inputCriar" /></td>
+                        </tr>
+                    </thead>
+                </table>
+
+                <button on:click={criar} on:click={read} on:click={() => trocarTela("dados")}>criar conta</button>
             </div>
 
-            <div>
+
+
+            <div id="acessar" style="display:{acessar};">
                 <h1>Acessar Conta</h1>
-                <input type="text" bind:value={userCode} /> <br />
-                <button on:click={read}>ver os dados do Mongo</button><br />
-                
-                <!-- <p>sacola : {JSON.stringify(resultado.sacola)}</p> -->
+
+                <div style="display:flex">
+                    <input id="inputAcesso" type="text" bind:value={userCode} />
+                    <br />
+                    <button
+                        id="buttonAcesso"
+                        on:click={read}
+                        on:click={() => trocarTela("dados")}
+                    >
+                        entrar
+                    </button>
+                </div>
+
+                <br />
             </div>
 
-            <div>
-                <hr>
+            <div id="dados" style="display:{dados};">
+                <hr />
                 <h1>Informações Pessoais</h1>
-                <p>nome : {resultado.nome}</p>
-                <p>descrição : {resultado.descricao}</p>
-                <p>conta : {resultado.identificador}</p>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <td>nome</td>
+                            <td>{resultado.nome}</td>
+                        </tr>
+                        <tr>
+                            <td>descrição</td>
+                            <td>{resultado.descricao}</td>
+                        </tr>
+                        <tr>
+                            <td>código de acesso</td>
+                            <td>{resultado.identificador}</td>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </article>
 
@@ -114,9 +180,82 @@
     }
 
     article {
-        min-height: 30vh;
+        min-height: 80vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         background-color: rgb(143, 189, 112);
         border-left: 1px black solid;
         border-right: 1px black solid;
+    }
+
+    #conta {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #criar {
+        display: none;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #acessar {
+        display: none;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #dados {
+        display: none;
+        align-items: center;
+        flex-direction: column;
+    }
+    table,
+    tr,
+    td {
+        background-color: white;
+        border: 1px solid rgb(110, 110, 110);
+        padding: 0.5em 2em;
+        font-family: monospace;
+        font-size: 30px;
+    }
+
+    table {
+        margin-bottom: 2em;
+        border-radius: 0.8em;
+    }
+
+    h1 {
+        font-size: 40px;
+        font-family: monospace;
+        color: rgb(255, 255, 255);
+    }
+
+    #inputAcesso {
+        width: 400px;
+        font-size: 1.4em;
+        padding: 0.8em;
+        border-radius: 10px 0 0 10px;
+        border: 0;
+    }
+
+    #buttonAcesso {
+        font-size: 1.4em;
+        padding: 0.8em;
+        display: flex;
+        border-radius: 0 10px 10px 0;
+        border: 0;
+        border-left: 2px solid rgb(212, 212, 212);
+        color: rgb(3, 3, 3);
+    }
+
+    .inputCriar{
+        width: 100%;
+        height: 2em;
+        padding: 0;
+        font-size: 1em;
+        border: 0;
     }
 </style>
